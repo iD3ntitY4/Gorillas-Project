a3257c90c53097077d14fdd37d7496c5a5e88d5a
HippoMagno <n.dycke@gmx.de>
2015-03-15 18:16:33 +0100

    Added some stuff for gorilla
    Actions, events

---
+++ src/de/tu_darmstadt/gdi1/gorillas/game/model/actions/EndOfRoundAction.java
@@ -0,0 +1,16 @@
+package de.tu_darmstadt.gdi1.gorillas.game.model.actions;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+import eea.engine.action.Action;
+import eea.engine.component.Component;
+
+public class EndOfRoundAction implements Action{
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sb, int delta, Component event) {
+		// TODO Add interaction with gameplay state		
+	}
+
+}
--- src/de/tu_darmstadt/gdi1/gorillas/game/model/entities/Gorilla.java
+++ src/de/tu_darmstadt/gdi1/gorillas/game/model/entities/Gorilla.java
@@ -1,6 +1,14 @@
 package de.tu_darmstadt.gdi1.gorillas.game.model.entities;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import de.tu_darmstadt.gdi1.gorillas.game.model.actions.EndOfRoundAction;
+import de.tu_darmstadt.gdi1.gorillas.game.model.actions.GorillaDanceAction;
+import de.tu_darmstadt.gdi1.gorillas.game.model.events.GorillaHitEvent;
+import eea.engine.component.render.ImageRenderComponent;
 import eea.engine.entity.*;
+import eea.engine.event.basicevents.TimeEvent;
 
 /**
  * This class represents the player figure, in fact a gorilla.
@@ -13,9 +21,49 @@ import eea.engine.entity.*;
  * @see eea.engine.entity.Entity
  */
 public class Gorilla extends Entity{
-		
-	public Gorilla(String id)
+	
+	private final String defaultImagePathNormal = ".\\assets\\gorillas\\gorillas\\gorilla_new.png";
+	private final String defaultImagePathRight = ".\\assets\\gorillas\\gorillas\\gorilla_new_right_up.png";
+	private final String defaultImagePathLeft = ".\\assets\\gorillas\\gorillas\\gorilla_new_left_up.png";
+	
+	public String[] imagePaths = {defaultImagePathNormal, defaultImagePathRight, defaultImagePathLeft};
+	
+	/**
+	 * Gorilla is constructed with a identification number, to differ the gorillas.
+	 * 
+	 * @param idNum is the id-number; should be unique.
+	 */
+	public Gorilla(int idNum)
 	{
-		super(id);
+		super("Gorilla" + Integer.toString(idNum) );
+		this.setImage(defaultImagePathNormal);
+		
+		//End of round event
+		GorillaHitEvent gorillaHit = new GorillaHitEvent();
+		TimeEvent danceAfterRound = new TimeEvent(100, false);//TODO: Replace false with a variable expressing a won round!
+				
+		//End of round Action
+		gorillaHit.addAction(new EndOfRoundAction());
+		danceAfterRound.addAction(new GorillaDanceAction()); //Problem: Gorilla kann auf falsches Bild enden; also muss "gelöscht" werden.
+		
+		//Add all events to gorilla
+		this.addComponent(gorillaHit);
+		this.addComponent(danceAfterRound);
 	}	
+	
+	/**
+	 * This method sets the image of the gorilla to the image of the given path.
+	 * 
+	 * @param pathPic should be given as a relative path; e.g.: .\\super\\name.png
+	 */
+	public void setImage (String pathPic)
+	{
+		try
+		{
+			this.addComponent(new ImageRenderComponent(new Image(pathPic)));
+		} catch(SlickException se)
+		{
+			se.printStackTrace();
+		}
+	}
 }
