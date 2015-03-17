ab3fd422e2993a3ade7005620c78bdc58407efd9
HippoMagno <n.dycke@gmx.de>
2015-03-17 22:22:47 +0100

    Forgot smth ...

---
+++ src/de/tu_darmstadt/gdi1/gorillas/game/model/events/BananaOutOfBoundsEvent.java
@@ -0,0 +1,22 @@
+package de.tu_darmstadt.gdi1.gorillas.game.model.events;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+import de.tu_darmstadt.gdi1.gorillas.game.model.World;
+import eea.engine.event.Event;
+
+public class BananaOutOfBoundsEvent extends Event {
+		
+	public BananaOutOfBoundsEvent() {
+		super("BananaBottomOutEvent");
+	}
+
+	@Override
+	protected boolean performAction(GameContainer gc, StateBasedGame sb, int delta) {
+		float yPos = this.getOwnerEntity().getPosition().getY();
+		float xPos = this.getOwnerEntity().getPosition().getX();
+		
+		return !(yPos <= World.worldHeight && xPos >= 0 && xPos <= World.worldWidth);
+	}
+}
